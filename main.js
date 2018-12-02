89becfe6155a594db6ab38910309e0d1428abba5
JmMcGehee <45159628+JmMcGehee@users.noreply.github.com>
2018-12-01 21:19:42 -0700

    Add files via upload
    Finished game. Needs improvements.

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-console.log("Up and running!");
+// PROBLEMS With GAME: 
+// 1.) No RESET BUTTON. 
+// 2.) You can click the same card twice. 
+// 3.) Second card doesn't flip over until after alert.
+// 4.) Cards don't shuffle. 
 
 var cards = [
 	{
@@ -29,25 +33,41 @@ var cardsInPlay = [];
 
 var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-	console.log("You found a match!");
+		alert("You found a match!");
 	} else {
-	console.log("Sorry, try again.");
+		alert("Sorry, try again.");
 // console.log needs to be changed to alert
 	}
 }
 
-var flipCard = function (cardId) {
+var flipCard = function () {
 	// I don't quite understand where these go. 
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank); 
+	
 	console.log(cards[cardId].cardImage);
+	
 	console.log(cards[cardId].suit);
 	// Somehow addng the two linse above up here got my checkForMatch function to run...
+	this.setAttribute('src', cards[cardId].cardImage);
 if (cardsInPlay.length === 2) {
 	checkForMatch();
 }
 }
 
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+    	var cardElement = document.createElement('img');
 
-flipCard(0);
-flipCard(2);
+    	cardElement.setAttribute('src' , 'images/back.png');
+
+    	cardElement.setAttribute('data-id' , i);
+    	cardElement.addEventListener('click' , flipCard);
+    	document.getElementById('game-board').appendChild(cardElement);
+
+    	
+    }
+}
+
+createBoard();
